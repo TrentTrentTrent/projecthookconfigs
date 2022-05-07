c2ab72d80dbea36d83c3b435ac8d5577893c0a1a
Trent <99286664+TrentTrentTrent@users.noreply.github.com>
2022-05-06 22:25:08 -0400

    Config Loader

---
+++ load.lua
@@ -0,0 +1,14 @@
+local configloader = {}
+configlinks = {
+    rage = "https://raw.githubusercontent.com/TrentTrentTrent/projecthookconfigs/main/configs/rage.txt",
+    ragenobow = "https://raw.githubusercontent.com/TrentTrentTrent/projecthookconfigs/main/configs/ragenobow.txt"
+}
+--
+function configloader:add(name,link,actualname)
+    if configlinks[actualname] ~= nil then
+        writefile("projecthook/"..name..".txt",game:HttpGet(configlinks[actualname]))
+    end
+end
+function configloader:delete(name)
+    delfile("projecthook/"..name..".txt")
+end
